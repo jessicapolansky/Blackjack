0e3a93dcb57574aa262274028cf82ad8e8b65ced
Jessica Robinson <jessica.polansky@gmail.com>
2018-04-21 23:16:17 +0000

    adding gameplay

--- blackjack.js
+++ blackjack.js
@@ -1,9 +1,17 @@
 $( document ).ready(function() {
     $('#deal-button').click(function () {
-        var shuffle = $.getJSON('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-        var deck = shuffle.responseJSON;
+        var shuffle = $.get('https://deckofcardsapi.com/api/deck/new/shuffle/?');
+        var deck = shuffle['responseJSON'];
         console.log(shuffle);
         console.log(deck);
-
     });
+    
+    $('#hit-button').click(function () {
+        var newCard = $.get('https://deckofcardsapi.com/api/gfp1gymo8aej/deck/draw/?count=1');
+        console.log(newCard);
+    });
+    
+    
 });
+
+
